1aa9c81ea755edb623146d426e538937585fa81f
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-03-22 23:00:06 +0800

    Update CWHovercraft.java

--- src/main/java/dev/hail/wulfrum/entity/CWHovercraft.java
+++ src/main/java/dev/hail/wulfrum/entity/CWHovercraft.java
@@ -1,14 +1,12 @@
 package dev.hail.wulfrum.entity;
 
-import dev.hail.wulfrum.WulfrumMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.EntitySelector;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.control.MoveControl;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
@@ -16,13 +14,16 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.ai.util.AirAndWaterRandomPos;
 import net.minecraft.world.entity.ai.util.HoverRandomPos;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.monster.Enemy;
+import net.minecraft.world.entity.monster.Phantom;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.phys.Vec3;
 import org.confluence.terraentity.entity.monster.AbstractMonster;
 import org.confluence.terraentity.entity.util.DeathAnimOptions;
@@ -35,10 +36,10 @@ import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 import javax.annotation.Nullable;
+import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.Logger;
 
 public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, DeathAnimOptions {
     protected int attackInternal = 20;
@@ -49,55 +50,26 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
 
     public CWHovercraft(EntityType<? extends CWHovercraft> entityType, Level level, Builder builder) {
         super(entityType, level, builder);
-        this.moveControl = new FlyingMoveControl(this, 20, false);
+        this.moveControl = new HMoveControl(this);
     }
 
-    @Override
-    public void travel(Vec3 travelVector) {
-        if (this.isControlledByLocalInstance()) {
-            if (this.isInWater()) {
-                this.moveRelative(0.02F, travelVector);
-                this.move(MoverType.SELF, this.getDeltaMovement());
-                this.setDeltaMovement(this.getDeltaMovement().scale(0.8F));
-            } else if (this.isInLava()) {
-                this.moveRelative(0.02F, travelVector);
-                this.move(MoverType.SELF, this.getDeltaMovement());
-                this.setDeltaMovement(this.getDeltaMovement().scale(0.5));
-            } else {
-                BlockPos ground = getBlockPosBelowThatAffectsMyMovement();
-                float f = 0.91F;
-                if (this.onGround()) {
-                    f = this.level().getBlockState(ground).getFriction(this.level(), ground, this) * 0.91F;
-                }
-
-                float f1 = 0.16277137F / (f * f * f);
-                f = 0.91F;
-                if (this.onGround()) {
-                    f = this.level().getBlockState(ground).getFriction(this.level(), ground, this) * 0.91F;
-                }
-
-                this.moveRelative(this.onGround() ? 0.1F * f1 : 0.02F, travelVector);
-                this.move(MoverType.SELF, this.getDeltaMovement());
-                this.setDeltaMovement(this.getDeltaMovement().scale(f));
-            }
-        }
-    }
     @Override
     protected void checkFallDamage(double y, boolean onGround, BlockState state, BlockPos pos) {
     }
 
     @Override
     protected void registerGoals() {
-        goalSelector.addGoal(1, new MeleeAttackGoal(this, 2, true));
-        //goalSelector.addGoal(2, new SweepAttackGoal());
-        //goalSelector.addGoal(3, new CircleAroundAnchorGoal());
-        goalSelector.addGoal(4, new KeepOnTargetGoal(this));
+        goalSelector.addGoal(1, new AttackStrategyGoal());
+        //goalSelector.addGoal(1, new MeleeAttackGoal(this, 0.5, false));
+        goalSelector.addGoal(2, new SweepAttackGoal());
+        goalSelector.addGoal(3, new CircleAroundAnchorGoal());
+        //goalSelector.addGoal(4, new KeepOnTargetGoal(this));
 
         registerTargetGoal(targetSelector);
     }
     protected void registerTargetGoal(GoalSelector targetSelector){
         targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, false));
+        targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     enum AttackPhase {
@@ -105,6 +77,51 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
         SWOOP
     }
 
+    class AttackStrategyGoal extends Goal {
+        private int nextSweepTick;
+
+        @Override
+        public boolean canUse() {
+            LivingEntity livingentity = getTarget();
+            return livingentity != null && canAttack(livingentity, TargetingConditions.DEFAULT);
+        }
+
+        @Override
+        public void start() {
+            this.nextSweepTick = this.adjustedTickDelay(10);
+            attackPhase = AttackPhase.CIRCLE;
+            this.setAnchorAboveTarget();
+        }
+
+        @Override
+        public void stop() {
+            anchorPoint = level()
+                    .getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, anchorPoint)
+                    .above(10 + random.nextInt(20));
+        }
+
+        @Override
+        public void tick() {
+            if (attackPhase == AttackPhase.CIRCLE) {
+                this.nextSweepTick--;
+                if (this.nextSweepTick <= 0) {
+                    attackPhase = AttackPhase.SWOOP;
+                    this.setAnchorAboveTarget();
+                    this.nextSweepTick = this.adjustedTickDelay((8 + random.nextInt(4)) * 20);
+                    playSound(SoundEvents.PHANTOM_SWOOP, 10.0F, 0.95F + random.nextFloat() * 0.1F);
+                }
+            }
+        }
+
+        private void setAnchorAboveTarget() {
+            anchorPoint = Objects.requireNonNull(getTarget()).blockPosition().above(20 + random.nextInt(20));
+            if (anchorPoint.getY() < level().getSeaLevel()) {
+                anchorPoint = new BlockPos(
+                        anchorPoint.getX(), level().getSeaLevel() + 1, anchorPoint.getZ()
+                );
+            }
+        }
+    }
     class CircleAroundAnchorGoal extends MoveTargetGoal {
         private float angle;
         private float distance;
@@ -113,7 +130,7 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
 
         @Override
         public boolean canUse() {
-            return getTarget() == null || attackPhase == AttackPhase.CIRCLE;
+            return attackPhase == AttackPhase.CIRCLE;
         }
 
         @Override
@@ -122,6 +139,7 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
             this.height = -4.0F + random.nextFloat() * 9.0F;
             this.clockwise = random.nextBoolean() ? 1.0F : -1.0F;
             this.selectNext();
+            setNoGravity(true);
         }
 
         @Override
@@ -169,9 +187,6 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
         }
     }
     class SweepAttackGoal extends MoveTargetGoal {
-        private static final int CAT_SEARCH_TICK_DELAY = 20;
-        private boolean isScaredOfCat;
-        private int catSearchTick;
 
         @Override
         public boolean canUse() {
@@ -182,31 +197,22 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
         public boolean canContinueToUse() {
             LivingEntity livingentity = getTarget();
             if (livingentity == null) {
+                setNoGravity(false);
                 return false;
             } else if (!livingentity.isAlive()) {
+                setNoGravity(false);
                 return false;
             } else {
                 if (livingentity instanceof Player player && (livingentity.isSpectator() || player.isCreative())) {
+                    setNoGravity(false);
                     return false;
                 }
 
                 if (!this.canUse()) {
+                    setNoGravity(false);
                     return false;
-                } else {
-                    if (tickCount > this.catSearchTick) {
-                        this.catSearchTick = tickCount + 20;
-                        List<Cat> list = level()
-                                .getEntitiesOfClass(Cat.class, getBoundingBox().inflate(16.0), EntitySelector.ENTITY_STILL_ALIVE);
-
-                        for (Cat cat : list) {
-                            cat.hiss();
-                        }
-
-                        this.isScaredOfCat = !list.isEmpty();
-                    }
-
-                    return !this.isScaredOfCat;
                 }
+                return true;
             }
         }
     }
@@ -219,6 +225,8 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
             return moveTargetPoint.distanceToSqr(getX(), getY(), getZ()) < 4.0;
         }
     }
+
+
     protected class KeepOnTargetGoal extends Goal {
         private final int FIND_PATH_TIME = 200;
         int timeToRepath;
@@ -230,7 +238,6 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
         }
 
         public boolean canUse() {
-
             return bee.getTarget() != null && bee.getTarget().isAlive() && (--timeToRepath <= 0 || timeToRepath < FIND_PATH_TIME - attackInternal);
         }
 
@@ -262,34 +269,12 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
         public void tick() {
             bee.lookControl.setLookAt(Objects.requireNonNull(bee.getTarget()));
             bee.lookAt(bee.getTarget(), 360, 360);
-            if(bee.distanceTo(bee.getTarget()) < 10){
-
-            }
         }
     }
 
     @Override
     public void tick() {
-        // TODO: 仇恨值
-        Vec3 pos = position();
-        /*LivingEntity livingentity = getTarget();
-        if (livingentity != null) {
-            moveTargetPoint = new Vec3(livingentity.getX(), livingentity.getY(0.5), livingentity.getZ());
-            if (getBoundingBox().inflate(0.2F).intersects(livingentity.getBoundingBox())) {
-                doHurtTarget(livingentity);
-                attackPhase = AttackPhase.CIRCLE;
-                if (!isSilent()) {
-                    level().levelEvent(1039, blockPosition(), 0);
-                }
-            } else if (horizontalCollision || hurtTime > 0) {
-                attackPhase = AttackPhase.CIRCLE;
-            }
-        }*/
-        //setTarget(level().getNearestPlayer(pos.x, pos.y, pos.z, 40, true));
         super.tick();
-        // 在super.tick()结束后更新面向方向即可覆盖原版AI
-        TEUtils.updateEntityRotation(this, this.getDeltaMovement().multiply(1, 1, 1));
-
     }
     @Override
     protected PathNavigation createNavigation(Level p_level) {
@@ -308,6 +293,53 @@ public class CWHovercraft extends AbstractMonster implements Enemy, GeoEntity, D
         return flyingpathnavigation;
     }
 
+    class HMoveControl extends MoveControl {
+        private float speed = 0.1F;
+
+        public HMoveControl(Mob mob) {
+            super(mob);
+        }
+
+        @Override
+        public void tick() {
+            if (horizontalCollision) {
+                setYRot(getYRot() + 180.0F);
+                this.speed = 0.1F;
+            }
+
+            double d0 = moveTargetPoint.x - getX();
+            double d1 = moveTargetPoint.y - getY();
+            double d2 = moveTargetPoint.z - getZ();
+            double d3 = Math.sqrt(d0 * d0 + d2 * d2);
+            if (Math.abs(d3) > 1.0E-5F) {
+                double d4 = 1.0 - Math.abs(d1 * 0.7F) / d3;
+                d0 *= d4;
+                d2 *= d4;
+                d3 = Math.sqrt(d0 * d0 + d2 * d2);
+                double d5 = Math.sqrt(d0 * d0 + d2 * d2 + d1 * d1);
+                float f = getYRot();
+                float f1 = (float)Mth.atan2(d2, d0);
+                float f2 = Mth.wrapDegrees(getYRot() + 90.0F);
+                float f3 = Mth.wrapDegrees(f1 * (180.0F / (float)Math.PI));
+                setYRot(Mth.approachDegrees(f2, f3, 4.0F) - 90.0F);
+                yBodyRot = getYRot();
+                if (Mth.degreesDifferenceAbs(f, getYRot()) < 3.0F) {
+                    this.speed = Mth.approach(this.speed, 1.8F, 0.005F * (1.8F / this.speed));
+                } else {
+                    this.speed = Mth.approach(this.speed, 0.2F, 0.025F);
+                }
+
+                float f4 = (float)(-(Mth.atan2(-d1, d3) * 180.0F / (float)Math.PI));
+                setXRot(f4);
+                float f5 = getYRot() + 90.0F;
+                double d6 = (double)(this.speed * Mth.cos(f5 * (float) (Math.PI / 180.0))) * Math.abs(d0 / d5);
+                double d7 = (double)(this.speed * Mth.sin(f5 * (float) (Math.PI / 180.0))) * Math.abs(d2 / d5);
+                double d8 = (double)(this.speed * Mth.sin(f4 * (float) (Math.PI / 180.0))) * Math.abs(d1 / d5);
+                Vec3 vec3 = getDeltaMovement();
+                setDeltaMovement(vec3.add(new Vec3(d6, d8, d7).subtract(vec3).scale(0.2)));
+            }
+        }
+    }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
